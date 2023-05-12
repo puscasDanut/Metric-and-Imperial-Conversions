5a0068add9ea68d9f1a7bcd03e519596e1fb48cb
puscasDanut <danielionut@puscas.xyz>
2023-05-12 13:19:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+if __name__ == '__main__':
+    window = Tk()
+    window.minsize(height=100, width=400)
+    window.title("Metric and Imperial Conversions")
+
+
+    window.mainloop()
