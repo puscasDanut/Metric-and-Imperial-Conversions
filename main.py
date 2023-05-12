6d8a899e0b4453089074a463dbd34e0808ecb6df
puscasDanut <danielionut@puscas.xyz>
2023-05-12 13:48:55 +0300

    Finished project, with no kwargs or args

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ if __name__ == '__main__':
     window.minsize(height=100, width=400)
     window.title("Metric and Imperial Conversions")
 
+    def calculate():
+        if initial_units["text"] == "Miles":
+            final_value.config(text=str("{:.2f}".format(float(input.get()) * 1.6)))
+        else:
+            final_value.config(text=str("{:.2f}".format(float(input.get()) / 1.6)))
+
+    def switch_units():
+        initial_units["text"], other_units["text"] = other_units["text"], initial_units["text"]
+        final_value.config(text="0")
+
+
     empty_label = Label(text="", padx=50)
     empty_label.grid(column=0, row=0)
 
@@ -22,6 +33,13 @@ if __name__ == '__main__':
 
     other_units = Label(text="Km")
     other_units.grid(column=2, row=1)
+
+    calculate_button = Button(text="Calculate", command=calculate)
+    calculate_button.grid(column=3, row=0, padx=10)
+
+    switch_button = Button(text="Switch Units", command=switch_units)
+    switch_button.grid(column=3, row=1)
+
 
 
     window.mainloop()
